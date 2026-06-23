4d85703b70c6467ee15c5aa57ff38e41787d31fa
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-06-23 09:52:56 +0530

    Array Learning

---
+++ src/course/variablepkg/ArrayTest.java
@@ -0,0 +1,34 @@
+package course.variablepkg;
+
+import course.mypackageB.Person;
+
+public class ArrayTest {
+    public static void main(String[] args){
+        Person[] users = new Person[3];
+        Person alex = new Person();
+        alex.name = "Alex";
+        alex.age= 55;
+        alex.gender= "Male";
+
+
+        Person mia = new Person();
+        mia.name="Mia";
+        mia.age=20;
+        mia.gender="Female";
+
+
+        Person vihan =new Person();
+        vihan.name="Vihan";
+        vihan.age=8;
+        vihan.gender="Male";
+
+        users[0] = alex;
+        users[1] = mia;
+        users[2] = vihan;
+
+        System.out.println(users[0].age);
+
+        }
+
+    }
+
