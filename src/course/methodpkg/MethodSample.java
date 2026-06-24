19d16aa523458a15d6389c62f708568f55d39015
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-06-24 10:54:23 +0530

    Methods

--- src/course/methodpkg/MethodSample.java
+++ src/course/methodpkg/MethodSample.java
@@ -1,4 +1,13 @@
 package course.methodpkg;
 
 public class MethodSample {
+    public static void main(String[]args){
+        MethodSample obj = new MethodSample();
+        obj.sayHello("male");
+
+    }
+    public void sayHello(String gender){
+        String newText = gender.equalsIgnoreCase("Male") ? "Hello Sir" : "Hello Madam";
+        System.out.println(newText);
+    }
 }
