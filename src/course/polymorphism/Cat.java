98722a67e74a214ae98e16abb93364090bd6a6ac
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-11 11:23:14 +0530

    Method Overloading

--- src/course/polymorphism/Cat.java
+++ src/course/polymorphism/Cat.java
@@ -1,6 +1,6 @@
 package course.polymorphism;
 
-public class Cat extends DomesticAnimal{
+public class Cat extends DomesticAnimal {
 
     @Override
     public void eat() {
@@ -8,11 +8,21 @@ public class Cat extends DomesticAnimal{
         System.out.println("I will eat Meat as well");
     }
 
-    public static void main(String[] args){
-        Cat c = new Cat();
-        c.setMinSpeed("24 km/h");
-        c.eat();
-        c.run();
+    public String run(boolean hasDanger) {
+        if (hasDanger) {
+            System.out.println("I am running at 48 km/h");
+        } else {
+            super.run();
+        }
+        return "Running";
 
     }
-}
+        public static void main (String[]args){
+            Cat c = new Cat();
+            c.setMinSpeed("24 km/h");
+            c.eat();
+            c.run(false);
+
+        }
+    }
+
