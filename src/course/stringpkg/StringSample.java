36523330539a1e9e75e906af38a88eee5455beb2
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-12 20:53:20 +0530

    Strings

--- src/course/stringpkg/StringSample.java
+++ src/course/stringpkg/StringSample.java
@@ -1,15 +1,24 @@
 package course.stringpkg;
 
 public class StringSample {
-    public static void main(String[] args){
-        String s1 = new String("Welcome");
-        String s2 = new String("Welcome");
+    public static void main(String[] args) {
+        String s1 = "Welcome to Java";
+        String s2 = "Welcome to Java";
+        System.out.println(s1.toUpperCase());
+        //System.out.println(s1.substring(1,3));
+        //System.out.println(s1.replace(" ","-"));--> replace
+        //System.out.println(s1.length()); --> Length
 
-        if(s1 == s2){
-            System.out.println("Both refrences are pointing to same object");
-
+        /*if(s1.equals(s2)){ --> equals
+            System.out.println("equal");
         }else{
-            System.out.println("Both references are pointing to different object");
-        }
+            System.out.println("Different");
+        }*/
+        //System.out.println(s1.charAt(0)); --> charAt method
+        //System.out.println(s1.concat("!!!!")); --> you can use + operator also
+        //System.out.println(s1 + "!!!"); -->concat method
     }
-}
+
+    }
+
+
