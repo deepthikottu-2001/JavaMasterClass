91d7cacb6148157d1b82d0e291cb0354de81741c
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-13 02:11:51 +0530

    String Buffer & Builder

--- src/course/stringpkg/StringSample.java
+++ src/course/stringpkg/StringSample.java
@@ -2,21 +2,13 @@ package course.stringpkg;
 
 public class StringSample {
     public static void main(String[] args) {
-        String s1 = "Welcome to Java";
-        String s2 = "Welcome to Java";
-        System.out.println(s1.toUpperCase());
-        //System.out.println(s1.substring(1,3));
-        //System.out.println(s1.replace(" ","-"));--> replace
-        //System.out.println(s1.length()); --> Length
-
-        /*if(s1.equals(s2)){ --> equals
-            System.out.println("equal");
-        }else{
-            System.out.println("Different");
-        }*/
-        //System.out.println(s1.charAt(0)); --> charAt method
-        //System.out.println(s1.concat("!!!!")); --> you can use + operator also
-        //System.out.println(s1 + "!!!"); -->concat method
+        //StringBuffer s1 = new StringBuffer("Welcome Java");
+        StringBuilder s1 = new StringBuilder("Welcome Java");
+        //s1.append("!!!"); --> append Method
+        //s1.charAt(1);
+        //s1.delete(1,3);
+        s1.insert(1,"aa");
+        System.out.println(s1);
     }
 
     }
