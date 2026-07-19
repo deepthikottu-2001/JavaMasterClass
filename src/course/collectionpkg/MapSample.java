87a1351eaefac33a7143d1a3598b7925feef6098
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-19 15:05:08 +0530

    Hashtable

--- src/course/collectionpkg/MapSample.java
+++ src/course/collectionpkg/MapSample.java
@@ -1,15 +1,17 @@
 package course.collectionpkg;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class MapSample {
     public static void main(String[] args){
-        HashMap<String,String> myMap = new HashMap<>();
+        Hashtable<String,String> myMap = new Hashtable<>();
         myMap.put("CN","China");
         myMap.put("IN","India");
         myMap.put("US","United States");
         myMap.put("UK","United Kingdom");
+        myMap.remove("CN");
         for(Map.Entry<String,String> map :myMap.entrySet()){
             System.out.println(map.getKey()+ ":" +map.getValue());
         }
