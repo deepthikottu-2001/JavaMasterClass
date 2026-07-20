0149420b393d9fa34dc9208683f0c361f32103fa
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-20 16:40:57 +0530

    Syncronization

--- src/course/threadpkg/MyThread.java
+++ src/course/threadpkg/MyThread.java
@@ -4,10 +4,20 @@ public class MyThread implements Runnable {
 
 
     @Override
-    public void run() {
-        for(int i=0;i<3;i++){
-            System.out.println("Thread is "+ Thread.currentThread().getName()+", i value is ::" +i);
+    //public synchronized void run() --> synchronized method
+    public void run(){
+        synchronized (this){
+            for(int i=0;i<3;i++){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Thread is "+ Thread.currentThread().getName()+", i value is ::" +i);
+            }
+
         }
+
     }
     public static void main(String[] args){
         MyThread obj = new MyThread();
