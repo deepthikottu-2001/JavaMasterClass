61b7d13090bf9ff48196ab97d869965804914751
Rayudu Ganesh <veera.ganesh.rayudu@ibm.com>
2026-07-20 13:15:45 +0530

    Multi Threading

--- src/course/threadpkg/MyThread.java
+++ src/course/threadpkg/MyThread.java
@@ -6,12 +6,19 @@ public class MyThread implements Runnable {
     @Override
     public void run() {
         for(int i=0;i<3;i++){
-            System.out.println("i value is :: " +i);
+            System.out.println("Thread is "+ Thread.currentThread().getName()+", i value is ::" +i);
         }
     }
     public static void main(String[] args){
         MyThread obj = new MyThread();
-        Thread t = new Thread(obj);
-        t.start();
+        Thread t1 = new Thread(obj);
+        t1.setName("A");
+        Thread t2 = new Thread(obj);
+        t1.setName("B");
+        Thread t3 = new Thread(obj);
+        t1.setName("C");
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
